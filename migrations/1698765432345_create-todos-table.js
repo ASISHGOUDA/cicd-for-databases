9c171159fd1734e0ee6e4c446fa7ef8c3dc7f5a2
ASISHKUMAR GOUDA <gouda0682@gmail.com>
2025-03-01 12:30:44 +0530

    Update 1698765432345_create-todos-table.js

--- migrations/1698765432345_create-todos-table.js
+++ migrations/1698765432345_create-todos-table.js
@@ -2,8 +2,7 @@ exports.up = (pgm) => {
   pgm.createTable('todos', {
     id: 'id',
     title: { type: 'varchar(255)', notNull: true },
-    completed: { type: 'boolean', notNull: true, default: false },
-    created_at: { type: 'timestamp', notNull: true, default: pgm.func('current_timestamp') },
+    completed: { type: 'boolean', notNull: true, default: false },    
   });
 };
 
